bb97320aac4fece39e2f58d4e2f1ea5e34fbd86d
Paulo Braga <pbraga55@gmail.com>
2025-08-20 19:26:12 -0700

    build inicial infra

---
+++ main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.43.0"
+    }
+  }
+  cloud {
+    organization = "NonationITServices"
+    workspaces {
+      name = "FinaltestWS"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+
+  }
+  skip_provider_registration = true
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "TerraformLabRG-Final"
+  location = "East US"
+}
+
+module "finaltestmodule" {
+  source  = "app.terraform.io/NonationITServices/finaltestmodule/azurerm"
+  version = "1.0.0"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  name                = "pb87sdf89mn"
+  environment          = "DEV"
+  # insert required variables here
+}
